f767499c54568b60fe9c40b44158c9f7cbb32ab5
Filip Petersson <fpetersson96@gmail.com>
2018-10-10 21:30:53 +0200

    Fixed the progressbar, now working properly

--- frontend/src/Components/ProgressBar/index.js
+++ frontend/src/Components/ProgressBar/index.js
@@ -9,53 +9,45 @@ class ProgressBar extends React.Component {
       percent: 0
     };
 
-    this.changeProgressState = this.changeProgressState.bind(this);
+    this.percentScrolled = this.percentScrolled.bind(this)
   }
 
   componentDidMount() {
-    function getDocHeight() {
-      const d = document;
-      return Math.max(
-        d.body.scrollHeight,
-        d.documentElement.scrollHeight,
-        d.body.offsetHeight,
-        d.documentElement.offsetHeight,
-        d.body.clientHeight,
-        d.documentElement.clientHeight
-      );
-    }
-
-    function percentScrolled() {
-      const winHeight =
-        window.innerHeight ||
-        (document.documentElement || document.body).clientHeight;
-      const docHeight = getDocHeight();
-      const scrollTop =
-        window.pageYOffset ||
-        (document.documentElement || document.body.parentNode || document.body)
-          .scrollTop;
-      const trackLength = docHeight - winHeight;
-      const pctScrolled = Math.round((scrollTop / trackLength) * 100);
-      console.log("Scrolled: " + pctScrolled + "%");
-    }
-
-    window.addEventListener(
-      "scroll",
-      function() {
-        percentScrolled();
-      },
+   window.addEventListener(
+     "scroll",
+      this.percentScrolled,
       false
-    );
-  } //end didmount
-  changeProgressState() {
-    //random value that goes into state. How to fetch data from percentScrolled?
-    const value = parseInt(Math.random() * 100, 10);
+   );
+ }
 
-    this.setState({
-      percent: value
-    });
+  getDocHeight = () =>  {
+    const d = document;
+    return Math.max(
+      d.body.scrollHeight,
+      d.documentElement.scrollHeight,
+      d.body.offsetHeight,
+      d.documentElement.offsetHeight,
+      d.body.clientHeight,
+      d.documentElement.clientHeight
+    );
   }
 
+  percentScrolled = () => {
+    const winHeight =
+      window.innerHeight ||
+      (document.documentElement || document.body).clientHeight;
+    const docHeight = this.getDocHeight();
+    const scrollTop =
+      window.pageYOffset ||
+      (document.documentElement || document.body.parentNode || document.body)
+        .scrollTop;
+    const trackLength = docHeight - winHeight;
+    const pctScrolled = Math.round((scrollTop / trackLength) * 100);
+
+    this.setState({
+      percent: pctScrolled
+     });
+  }
   render() {
     return (
       <div>
@@ -65,9 +57,6 @@ class ProgressBar extends React.Component {
           strokeColor="#e1ab99"
           trailColor="#fff"
         />
-      <div>
-          <button onClick={this.changeProgressState}>Change State</button>
-        </div>
       </div>
     );
   }
